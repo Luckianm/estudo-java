ae1d13c15c447a4c4b63399ba8b60565927d9c78
Gustavo <luckianm@outlook.com>
2024-06-19 10:16:03 -0300

    Types of variables

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,4 +1,12 @@
 package br.com.lojinha;
 
 public class LojinhaApp {
+    public static void main(String[] args) {
+        String joias = "100gr de ouro 50k";
+        int minhaIdade = 29;
+        double meuSalario = 29500.99;
+        boolean minhaResposta = true;
+
+        System.out.println(joias);
+    }
 }
